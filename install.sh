839d14fd5daa32154413ab92bc386c6cfb68d9f6
Loïck Virot <lvirot@pm.me>
2023-04-26 22:11:41 +0200

    move folders

--- install.sh
+++ install.sh
@@ -9,3 +9,30 @@ else
   echo "=== .env file already exists, skipping."
 fi
 
+docker_compose_ids=$(docker-compose ps -q)
+if [[ $? != 0 ]]; then
+  echo "ERR: Cannot run docker-compose."
+elif [[ $docker_compose_ids ]]; then
+  echo "=== Started containers has been detected in docker-compose, skipping."
+else
+  # Run docker containers
+  echo "=== Starting Docker containers..."
+  docker-compose up -d --build
+fi
+
+echo "=== Check Symfony dependencies"
+docker-compose exec symfony symfony check:requirements
+if [[ $? != 0 ]]; then
+  exit $?
+fi
+
+echo "=== Remove www/.gitkeep"
+rm www/.gitkeep
+
+echo "=== Create new Symfony project"
+docker-compose exec symfony composer create-project symfony/skeleton .
+if [[ $? != 0 ]]; then
+  exit $?
+fi
+
+exit 0
